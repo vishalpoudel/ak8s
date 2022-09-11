ef063a0040455924387e4fc32195444d4fd911d6
vishal.poudel <vishal.poudel@ibm-vishalpoudel>
2022-09-11 17:05:52 +0530

    comm1

---
+++ ak8sTest/src/pageobjects/MyPageObject.java
@@ -0,0 +1,17 @@
+package pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@Page( title="My Page Object"                                
+     , summary=""
+     , relativeUrl=""
+     , connection="comm1"
+     )             
+public class MyPageObject {
+			
+}
